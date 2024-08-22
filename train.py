fba7e386a0b16959cc72adb4a763e07adba79fbe
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-22 14:43:57 +0200

    test

--- train.py
+++ train.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
     #optimizer = ihtSGD(net.parameters(), beta=10.0,sparsity=0.95, momentum=0.9,device=device,model=net)
     
     ## IHT-AGD
-    optimizer = ihtAGD(net.parameters(), beta=10.0,kappa=30.0,sparsity=0.95,device=device,model=net)
+    optimizer = ihtAGD(net.parameters(), beta=12.5,kappa=30.0,sparsity=0.95,device=device,model=net)
 
 
     # 
